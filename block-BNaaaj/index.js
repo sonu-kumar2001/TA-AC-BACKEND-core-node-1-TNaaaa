fe17b9d645bcd3a872943d7d0c68bbc84fdd8f21
sonu <sonurocckkerr@gmail.com>
2020-11-23 15:31:42 +0530

    refactored Assignment 1

--- block-BNaaaj/index.js
+++ block-BNaaaj/index.js
@@ -1,15 +1,15 @@
 console.log('welcome to nodejs');
 
 let os = require('os');
+let cpus = os.cpus().length;
+let freemem = os.freemem();
+let uptime = os.uptime();
+let version = os.version()
+console.log(cpus,freemem,uptime,version);
 
-console.log(os.cpus(),os.freemem(),os.uptime(),os.version());
+let {readFile,unlink,readFileSync} = require('fs');
 
-let fs = require('fs');
-fs.readFile('./content.md',(err,content) => {
-    if (err) throw err;
-    console.log(content.toString())
-});
-fs.unlink('./content.md', (err) => {
-    if (err) throw err;
-    console.log('content.md was deleted');
-  });
+readFileSync('./app.js');
+readFile('./app.js',(err,content)=> {
+    console.log(err.content);
+})
